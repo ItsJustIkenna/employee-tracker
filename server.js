f6e027915b3ba03cf412211bcddd2eec329ead98
Ikenna Nwagbara <ikenna.nwagbara@gmail.com>
2021-01-22 17:22:08 -0500

    added express server and database

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+const app = express();
+
+const PORT = 8080;
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "employee_db",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  connection.end();
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
